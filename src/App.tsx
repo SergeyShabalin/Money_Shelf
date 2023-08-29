3f3f7898f2c69de1cd29c44f09e7efe45b3b9209
lock_user <obaldevshiy@yandex.ru>
2023-08-29 13:22:00 +0300

    code-splitting lazy suspence

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, {Suspense} from 'react';
+import {Link, Route, Routes} from 'react-router-dom';
 import Counter from "./components/Counter";
 import './index.scss'
+import MainPage from "./pages/MainPage/MainPage";
+import AboutPage from "./pages/AboutPage/AboutPage";
+import {MainPageAsync} from "./pages/MainPage/MainPage.async";
+import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
 
 const App = () => {
     return (
         <div className='app'>
-            <Counter/>
+            <Link to={'/'}>Счетчик</Link>
+            <Link to={'/main'}>Главная</Link>
+            <Link to={'/about'}>О странице</Link>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    <Route path='/' element={<Counter/>}/>
+                    <Route path='/main' element={<MainPageAsync/>}/>
+                    <Route path='/about' element={<AboutPageAsync/>}/>
+                </Routes>
+            </Suspense>
         </div>
     );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,13 @@ import {someF} from "./test";
 import {render} from "react-dom";
 import React from "react";
 import App from "./App";
+import {BrowserRouter} from "react-router-dom";
 
 
 someF('111')
 render(
-    <App/>,
+    <BrowserRouter>
+        <App/>
+    </BrowserRouter>,
     document.getElementById('root')
 )
